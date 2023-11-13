92031110fa9caea1a925d85ef94f3af38b74abb4
Botond Zih <zihbot@gmail.com>
2023-11-13 21:55:03 +0100

    wifi

--- wifi/wifi.ino
+++ wifi/wifi.ino
@@ -1,8 +1,55 @@
-void setup() { pinMode(D6, OUTPUT); }
+#include <ESP8266WiFi.h>
+
+WiFiServer server(80);
+
+void setup() {
+  Serial.begin(115200);
+
+  Serial.println(WiFi.softAP("ESPsoftAP_01") ? "Ready" : "Failed!");
+  // WiFi.mode(WIFI_STA);
+
+  Serial.println("");
+  Serial.print("Soft-AP IP address = ");
+  Serial.println(WiFi.softAPIP());
+
+  server.begin();
+}
 
 void loop() {
-  digitalWrite(D6, HIGH);  // turn the LED on (HIGH is the voltage level)
-  delay(1000);             // wait for a second
-  digitalWrite(D6, LOW);   // turn the LED off by making the voltage LOW
-  delay(1000);             // wait for a second
+  WiFiClient client = server.available();
+
+  if (client) {
+    Serial.println("New Client.");
+    String currentLine = "";
+    while (client.connected()) {
+      if (client.available()) {
+        char c = client.read();
+        if (c == '\n') {
+          if (currentLine.length() == 0) {
+            client.println("HTTP/1.1 200 OK");
+            client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println();
+            client.println("<!DOCTYPE html><html>");
+            client.println(
+                "<head><meta name=\"viewport\" content=\"width=device-width, "
+                "initial-scale=1\">");
+            client.println("<link rel=\"icon\" href=\"data:,\">");
+            client.println("<body><h1>ESP32 Web Server</h1>");
+            client.println("</body></html>");
+            client.println();
+          } else {
+            Serial.println(currentLine);
+            currentLine = "";
+          }
+        } else if (c != '\r') {
+          currentLine += c;
+        }
+      }
+      client.stop();
+      Serial.println("Client disconnected.");
+      Serial.println("");
+    }
+    delay(1000);
+  }
 }
