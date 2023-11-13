225f51ccaa78ff98238e2509e75117f00c655643
Botond Zih <zihbot@gmail.com>
2023-11-13 22:02:44 +0100

    Arduino config

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,6 +1,10 @@
-void setup() { pinMode(A0, OUTPUT); }
+void setup() {
+  pinMode(A0, OUTPUT);
+  Serial.begin(9600);
+}
 
 void loop() {
+  Serial.println("Hello World");
   digitalWrite(A0, HIGH);
   delay(500);
   analogWrite(A0, LOW);
